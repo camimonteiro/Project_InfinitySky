3a44948e6ce4cae872b7a5c77a8e6ac3d787889b
Camille Monteiro <camillemonteiro.dev@gmail.com>
2022-02-26 20:59:18 -0300

    docs: att do arquivo de script

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ function apod (){
             conteudo.css("display", "flex");
             tituloNasa.text(`Erro na busca da Api`);
             textoNasa.text(`Insira uma data entre 16 de junho de 1995 (exceto os dias 17, 18 e 19 de junho de 1995) e o dia presente!`);
-            // imagemNasa.attr("src", "");
+            imagemNasa.attr("src", "./img/page_not_found.jpg");
             imagemNasa.css("width", "150px");
             videoNasa.css("display", "none");
         },
